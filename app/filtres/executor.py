ca74434ec9c7dc4557fd34846b747617ccd3f3c0
MaHryCT3 <mahryct123@gmail.com>
2023-07-18 22:04:06 +0500

    fix logs

--- app/filtres/executor.py
+++ app/filtres/executor.py
@@ -8,6 +8,6 @@ from app.filtres.abc import ABCFilter
 def execute_filters(data, filters: list[ABCFilter]) -> bool:
     for filter in filters:
         if not filter(deepcopy(data)):
-            logger.debug(f'{filter.__name__}: {data} is false')
+            logger.debug(f'{filter}: {data} is false')
             return False
     return True
